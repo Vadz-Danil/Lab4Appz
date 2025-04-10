319cc733bc8af8cd90903e93ea8a532d243e23c0
Vadz-Danil <vadz.danil@gmail.com>
2025-04-10 21:00:52 +0300

    add minor changes

---
+++ SightSeeing.Abstraction/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SightSeeing.Abstraction.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id, bool eager = false);
+        Task<IEnumerable<T>> GetAllAsync(bool eager = false);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
